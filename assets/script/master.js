eab2ec714f1ddeae31202bb8d535bc9451308ca3
Tarun Khathuriya <tarunkhathuriya@gmail.com>
2021-01-10 22:35:29 +0530

    nav styling done & nav child added & styling done

--- assets/script/master.js
+++ assets/script/master.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     $('.aside_angle').click(function(){
+        $(this).toggleClass('rotation');
         $('aside').toggleClass('expanded');
         $('header').toggleClass('expanded');
         $('.template').toggleClass('expanded');
+    });
+
+    // $('nav').mouseover(function(){
+    //     console.log('mouseenter');
+    //     $('aside').css('width', '265px');
+    // });
+
+    $('.has_childs').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('child_expanded');
     });
 });
